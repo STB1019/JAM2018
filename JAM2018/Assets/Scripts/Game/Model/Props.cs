bf20eb42b4cd648d64fcc1551ea34007a00acfce
DAGGOTH\massi <massimobono1@gmail.com>
2017-10-05 10:50:35 +0200

    Created Props and Qualitas classes
    - concrete props object and inheritance has not been implemented though

---
+++ JAM2018/Assets/Scripts/Game/Model/Props.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Scripts.Game.Model
+{
+
+	/// <summary>
+	/// Represents a static class containing all the props you can hacve in the maze
+	/// </summary>
+	public static class Props
+	{
+		public static readonly IList<IPropType> ALL = {
+			new DefaultPropType ("Fountain"),
+			new DefaultPropType ("Chest")
+		};
+	}
+}
+
