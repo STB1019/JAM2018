1137bb3234138475b55a25a95d1055b7f436b339
DAGGOTH\massi <massimobono1@gmail.com>
2017-10-05 11:42:43 +0200

    Simplify prop and qualitas creation
    - see the fountain example to see how you need to create a new prop (or qualitas)
    - added fountain script as an example

---
+++ JAM2018/Assets/Scripts/Game/Model/QualitasTypes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Scripts.Game.Model
+{
+	public static class QualitasTypes
+	{
+		public static IList<IQualitasType> ALL { get; private set; }
+
+		static QualitasTypes() {
+			ALL = new List<IPropType> ();
+		}
+
+		public void Add(string name) {
+			ALL.Add (new DefaultQualitasType (name));
+		}
+
+		public bool Contains(IQualitasType type) {
+			foreach (var t in ALL) {
+				if (t.Equals(type)) {
+					return true;
+				}
+			}
+		}
+
+		public bool Contains(string type) {
+			foreach (var t in ALL) {
+				if (t.Name.Equals(type)) {
+					return true;
+				}
+			}
+		}
+	}
+}
+
