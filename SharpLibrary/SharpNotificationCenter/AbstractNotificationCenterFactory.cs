e7d00707fbea52d9062d117f1359fc1a1c66041d
DAGGOTH\massi <massimobono1@gmail.com>
2017-10-05 09:46:48 +0200

    Created notification center
    - small test case implemented as well

---
+++ SharpLibrary/SharpNotificationCenter/AbstractNotificationCenterFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpNotificationCenter
+{
+	/// <summary>
+	/// Represents an abstract factory used to build every component needed inside a <see cref="NotificationCenter"/>
+	/// </summary>
+	public abstract class AbstractNotificationCenterFactory
+	{
+		/// <summary>
+		/// Method called whenever a <see cref="NotificationCenter"/> needs to create a new instance of <see cref="INotification"/>
+		/// </summary>
+		/// <param name="source">the object that requested to create a notification to dispatch with the notification center</param>
+		/// <param name="type">the topic of the notification</param>
+		/// <param name="data">an optional dictionary used to attach data to the notification</param>
+		/// <returns>an instance of a notification</returns>
+		public abstract INotification BuildNotification(object source, int type, IDictionary<int, object> data);
+	}
+}
---
+++ SharpLibrary/SharpNotificationCenter/DefaultNotification.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpNotificationCenter
+{
+	/// <summary>
+	/// A concrete implementation of <see cref="INotification"/>. This concrete class is used by default inside a <see cref="NotificationCenter"/>
+	/// </summary>
+	internal class DefaultNotification : INotification
+	{
+		/// <summary>
+		/// The topic of the notification
+		/// </summary>
+		public int Type { get; private set; }
+		/// <summary>
+		/// Represents the object that requested to send a notification by the notification center
+		/// </summary>
+		public object Source { get; private set; }
+		/// <summary>
+		/// An optional payload of data to send with the message
+		/// </summary>
+		public IDictionary<int, object> Data { get; private set; }
+
+		public DefaultNotification(object source, int type, IDictionary<int, object> data)
+		{
+			this.Source = source;
+			this.Type = type;
+			this.Data = data;
+		}
+
+		public bool IsEmpty { get { return this.Data.Count == 0; } }
+
+		public int DataSize { get
+			{
+				return this.Data.Count;
+			}
+			}
+
+		public bool Contains(int key)
+		{
+			return this.Data.ContainsKey(key);
+		}
+
+		public object Get(int key)
+		{
+			return this.Data[key];
+		}
+
+		public object this[int key] {
+			get {
+				return this.Get(key);
+			}
+		}
+	}
+}
