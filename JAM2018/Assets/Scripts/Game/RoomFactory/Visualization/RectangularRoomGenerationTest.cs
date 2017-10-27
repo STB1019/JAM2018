b2875b3d0b8a83f5249fb88fdf05a185dca56103
MicheleDusi <m.dusi007@studenti.unibs.it>
2017-10-27 15:14:30 +0200

    First implementatio of room creator and visualizer
    - Created the room maker which positions and visualizes tiles.
    - Dependecies with 3D models have to be verified.
    - Textures have to be implemented yet
    - DoorPlacer has to be implemented (Issue #30).

---
+++ JAM2018/Assets/Scripts/Game/RoomFactory/Visualization/RectangularRoomGenerationTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AssemblyCSharp;
+
+public class RectangularRoomGenerationTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject.Find ("RoomSeed").GetComponent<RectangularRoomGenerator>()
+			.GenerateRectangularRoomIn(this.gameObject, 5, 2, 6, RoomCoordinatesSystem.ShapeCentered);
+	}
+}
---
+++ JAM2018/Assets/Scripts/Game/RoomFactory/Visualization/SwitchCaseUnhandledException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	public class SwitchCaseUnhandledException : Exception
+	{
+		public SwitchCaseUnhandledException () : base()
+		{
+
+		}
+
+		public SwitchCaseUnhandledException (String message) : base(message)
+		{
+
+		}
+	}
+}
+
