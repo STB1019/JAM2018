9c04d2ebc1a53ce4c08b5d97609aaf1dd9bc0b54
DAGGOTH\massi <massimobono1@gmail.com>
2017-11-09 21:29:17 +0100

    jump script uses tags now WIP

---
+++ JAM2018/Assets/Scripts/Game/Commons/Tags.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Game.Commons
+{
+	/// <summary>
+	/// Represents a collection of every tag inside project JAM2018
+	/// </summary>
+	public class Tags
+	{
+		/// <summary>
+		/// Every gameobject tagged with this string is by default a terrain
+		/// </summary>
+		/// <remarks>Colliding with a terrain allows the player to jump</remarks>
+		public const string TERRAIN = "Terrain";
+	}
+}
