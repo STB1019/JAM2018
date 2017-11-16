5e178c5fa5b764c44e75ec5cacd9bad5e381b89d
Nikita Brancatisano <33670112+thevinter@users.noreply.github.com>
2017-11-16 08:54:07 +0100

    properly working Jump Script
    Jump script working.
    
    - Attach it to an empty gameobject and recall the isGrounded bool in the object you want to be jumping.

--- JAM2018/Assets/Scripts/Game/Controls/JumpScript.cs
+++ JAM2018/Assets/Scripts/Game/Controls/JumpScript.cs
@@ -23,9 +23,6 @@ namespace Scripts.Game.Controls {
     ///To properly use the script attach it to an empty GameObject located in the center of the player collider.
     ///Then reference to its "isGounded" variable in the PlayerController script to allow the jump.
     ///The variable height should be set to the Player's collider height
-	
-    ///This specific code has an inner jump script, so it will make jump specifically the object it's attached to
-    ///We will need to change that when working with more complex colliders.
 
 
 
@@ -47,18 +44,11 @@ namespace Scripts.Game.Controls {
             layerMask = ~layerMask; //We reverse the layer excluding every object besides those whose layer is 8
             Rigidbody rb = GetComponent<Rigidbody>();
             Vector3 colliderOrigin = transform.position;
+            Debug.Log(colliderOrigin);
 
             if (Physics.Raycast(colliderOrigin, Vector3.down, (height/2) + 0.5f, layerMask))
             {
-                Debug.Log("hmmmmm");
                 isGrounded = true;
-            }
-            if (Input.GetKeyDown(KeyCode.S) && isGrounded==true)
-            {
-                Debug.Log("I'm Jumping");
-                Vector3 jforce = new Vector3(0, jumpForce, 0);
-                rb.AddForce(jforce, ForceMode.Impulse);
-                isGrounded=false;
             }
         }
     }
