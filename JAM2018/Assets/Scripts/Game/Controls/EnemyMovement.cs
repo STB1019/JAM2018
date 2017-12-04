3d721aa428e2b01dacd002049424ea518f785bac
Nikita Brancatisano <theniki98@gmail.com>
2017-12-04 12:04:31 +0100

    Updated Enemy Movement script
    -Added comments
    -Removed some debugs

--- JAM2018/Assets/Scripts/Game/Controls/EnemyMovement.cs
+++ JAM2018/Assets/Scripts/Game/Controls/EnemyMovement.cs
@@ -1,49 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyMovement : MonoBehaviour
+namespace Scripts.Game.Controls
 {
-    Transform player;               
-    UnityEngine.AI.NavMeshAgent nav;         
-    private float distance;
-    public float viewDistance = 5;
-    public float attackDistance = 1.5f;
-          
-
-
-
-    void Awake ()
+    public class EnemyMovement : MonoBehaviour
     {
-        player = GameObject.FindGameObjectWithTag ("Player").transform;
-        nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+        private Transform player;
+        private UnityEngine.AI.NavMeshAgent nav;
+        private float distance;
+        public float viewDistance = 5;
+        public float attackDistance = 1.5f;
+
+        void Awake()
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+            nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        }
+
+        void Update()
+        {
+            Movement();
+        }
+
+        /// <summary>
+        /// This method updates the NavMesh Agent thus allowing the enemy to move.
+        /// If the player is within the enemy's range then the enemy will move towards
+        /// the player. Otherwhise he will stop. If the player is within melee range
+        /// then the enemy will always face the player.
+        /// </summary>
+        void Movement()
+        {
+            distance = Vector3.Distance(player.position, transform.position);
+            if (distance < viewDistance && distance > attackDistance)
+            {
+                Debug.Log("Player is in range and the enemy is following him");
+                nav.SetDestination(player.position);
+                nav.Resume();
+            }
+
+            else if (distance > viewDistance)
+            {
+                Debug.Log("The enemy is too far to see the player");
+                nav.Stop(true);
+            }
+
+            else if (distance < attackDistance)
+            {
+                Debug.Log("The enemy is close enough to attack");
+                nav.Stop(true);
+                transform.LookAt(player.transform);
+            }
+            else
+            {
+                Debug.Log("There's an error");
+            }
+        }
     }
-
-
-    void Update ()
-    {
-        distance = Vector3.Distance(player.position, transform.position);
-        if (distance < viewDistance && distance > attackDistance)
-        {
-            Debug.Log("Activating mesh");
-            nav.SetDestination (player.position);
-            nav.Resume();
-        }
-
-        else if (distance > viewDistance)
-        {
-            Debug.Log("Far away");
-            nav.Stop(true);
-        }
-
-        else if (distance < attackDistance)
-        {
-            Debug.Log("I'm Close"); 
-            nav.Stop(true);
-            transform.LookAt(player.transform);
-        }
-        else
-        {
-            Debug.Log("Other");
-        }
-    } 
 }
